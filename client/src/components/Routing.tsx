f4a061e2137b75c7df9ac196a6f3a24a45f4db23
Samir Khairati <samirk10101@gmail.com>
2025-03-08 15:35:35 +0530

    feat(phd): updated routing

--- client/src/components/Routing.tsx
+++ client/src/components/Routing.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@/hooks/Auth";
 import AdminLayout from "@/layouts/Admin";
 import QpReviewLayout from "@/layouts/QpReview";
+import PhdLayout from "@/layouts/Phd/Phd";
 import Admin from "@/views/Admin";
 import MembersView from "@/views/Admin/Members";
 import MemberDetailsView from "@/views/Admin/Members/[member]";
@@ -11,11 +12,22 @@ import FicSubmissionView from "@/views/QpReview/FicSubmission";
 import { permissions } from "lib";
 import { Computer, FileText } from "lucide-react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Phd from "@/views/Phd";
+import UpdateGrade from "@/views/Phd/NotionalSupervisor/UpdateGrade";
+import CourseworkForm from "@/views/Phd/DrcConvenor/CourseworkForm";
+import UpdateQualifyingExamDeadline from "@/views/Phd/DrcConvenor/UpdateQualifyingExamDeadline";
+import FormDeadline from "@/views/Phd/Student/FormDeadline";
 
 const adminModulePermissions = [
   permissions["/admin/member/search"],
   permissions["/admin/member/details"],
   permissions["/admin/role"],
+];
+
+const phdModulePermissions = [
+  permissions["notional-supervisor"],
+  permissions["drc-convenor"],
+  permissions["phd-student"],
 ];
 
 const qpReviewModulePermissions: string[] = [];
@@ -95,6 +107,34 @@ const Routing = () => {
                   element={<Navigate to="/qpReview/ficSubmission" />}
                 />
                 <Route path="ficSubmission" element={<FicSubmissionView />} />
+              </Route>
+            )}
+
+            {checkAccessAnyOne(phdModulePermissions) && (
+              <Route path="/phd" element={<PhdLayout />}>
+                <Route index element={<Phd />} />
+                {checkAccess(permissions["notional-supervisor"]) && (
+                  <>
+                    <Route path="update-grade" element={<UpdateGrade />} />
+                  </>
+                )}
+                {checkAccess(permissions["drc-convenor"]) && (
+                  <>
+                    <Route
+                      path="coursework-form"
+                      element={<CourseworkForm />}
+                    />
+                    <Route
+                      path="update-qualifying-exam-deadline"
+                      element={<UpdateQualifyingExamDeadline />}
+                    ></Route>
+                  </>
+                )}
+                {checkAccess(permissions["phd-student"]) && (
+                  <>
+                    <Route path="form-deadline" element={<FormDeadline />} />
+                  </>
+                )}
               </Route>
             )}
           </>
