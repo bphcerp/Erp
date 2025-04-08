6404fce3e228f613cb44dd3d11f00d84967391ea
Shiv Patil <krymzin0311@gmail.com>
2025-04-08 13:46:50 +0530

    chore(server/conference): add more checks to review field route

--- server/src/api/conference/fields/review/[type][id].ts
+++ server/src/api/conference/fields/review/[type][id].ts
@@ -10,8 +10,11 @@ import { asyncHandler } from "@/middleware/routeHandler.ts";
 import express from "express";
 import { authUtils, conferenceSchemas } from "lib";
 import { checkAccess } from "@/middleware/auth.ts";
-import { eq } from "drizzle-orm";
-import { areAllFieldsApprovedForApplication } from "@/lib/conference/index.ts";
+import { and, eq } from "drizzle-orm";
+import {
+    areAllFieldsApprovedForApplication,
+    getApplicationById,
+} from "@/lib/conference/index.ts";
 import { conferenceApprovalApplications } from "@/config/db/schema/conference.ts";
 
 const router = express.Router();
@@ -47,34 +50,63 @@ router.post(
             userEmail: req.user!.email,
         };
 
-        let fieldStatusQuery, fieldStatus, fieldStatusCol;
+        let fieldStatusQuery, fieldStatus, fieldStatusCol, fieldTableQuery;
 
         switch (type) {
             case "text":
                 fieldStatusQuery = db.query.textFieldStatus;
                 fieldStatus = textFieldStatus;
                 fieldStatusCol = textFieldStatus.textField;
+                fieldTableQuery = db.query.textFields;
                 break;
             case "number":
                 fieldStatusQuery = db.query.numberFieldStatus;
                 fieldStatus = numberFieldStatus;
                 fieldStatusCol = numberFieldStatus.numberField;
+                fieldTableQuery = db.query.numberFields;
                 break;
             case "date":
                 fieldStatusQuery = db.query.dateFieldStatus;
                 fieldStatus = dateFieldStatus;
                 fieldStatusCol = dateFieldStatus.dateField;
+                fieldTableQuery = db.query.dateFields;
                 break;
             case "file":
                 fieldStatusQuery = db.query.fileFieldStatus;
                 fieldStatus = fileFieldStatus;
                 fieldStatusCol = fileFieldStatus.fileField;
+                fieldTableQuery = db.query.fileFields;
                 break;
             default:
                 return next(
                     new HttpError(HttpCode.BAD_REQUEST, "Invalid field type")
                 );
         }
+
+        const field = await fieldTableQuery.findFirst({
+            where: (cols) => eq(cols.id, id),
+        });
+
+        if (!field)
+            return next(new HttpError(HttpCode.NOT_FOUND, "Field not found"));
+
+        const application = await getApplicationById(applId);
+        if (
+            !application ||
+            !Object.values(application.conferenceApplication)
+                .map((v) => (typeof v === "string" ? undefined : v?.id))
+                .includes(id)
+        )
+            return next(
+                new HttpError(HttpCode.NOT_FOUND, "Application not found")
+            );
+        if (application.status !== "pending")
+            return next(
+                new HttpError(
+                    HttpCode.BAD_REQUEST,
+                    "Application is already reviewed"
+                )
+            );
 
         const latestStatus = await fieldStatusQuery.findFirst({
             columns: {
@@ -119,14 +151,23 @@ router.post(
         }
 
         await db.insert(fieldStatus).values(value);
-        if (status && (await areAllFieldsApprovedForApplication(applId))) {
+        if (status && (await areAllFieldsApprovedForApplication(application))) {
             await db
                 .update(conferenceApprovalApplications)
                 .set({
                     state: conferenceSchemas.states[1],
                 })
                 .where(
-                    eq(conferenceApprovalApplications.applicationId, applId)
+                    and(
+                        eq(
+                            conferenceApprovalApplications.applicationId,
+                            applId
+                        ),
+                        eq(
+                            conferenceApprovalApplications.state,
+                            conferenceSchemas.states[0]
+                        )
+                    )
                 );
         } else if (!status) {
             await db
@@ -135,7 +176,9 @@ router.post(
                     state: conferenceSchemas.states[0],
                 })
                 .where(
-                    eq(conferenceApprovalApplications.applicationId, applId)
+                    and(
+                        eq(conferenceApprovalApplications.applicationId, applId)
+                    )
                 );
         }
 
