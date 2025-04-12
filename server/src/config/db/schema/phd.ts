2c1d6eac5f2d306b930e7b0eb5e0fab776c6b44d
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2025-04-12 18:07:45 +0530

    feat(server): added tables for different courses

--- server/src/config/db/schema/phd.ts
+++ server/src/config/db/schema/phd.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, serial, timestamp, integer } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, timestamp, integer, real } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 import { phd, users } from "./admin.ts";
 
@@ -59,4 +59,135 @@ export const phdExaminer = pgTable("phd_examiner", {
     examiner: text("examiner").references(() => users.email, {
         onDelete: "restrict",
     }),
+});
+
+export const phdCourseSeminar = pgTable("phd_course_seminar", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+    
+});
+
+export const phdCourseIndependentStudy = pgTable("phd_course_independent_study", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+    
+});
+
+export const phdCourseTeachingPractice1 = pgTable("phd_course_teaching_practice_1", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+    
+});
+
+export const phdCoursePracticeLectureSeries1 = pgTable("phd_course_practice_lecture_series_1", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+    
+});
+
+export const phdCourseThesis = pgTable("phd_course_thesis", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemGrade: text("end_sem_grade"),
+});
+
+export const phdCourseResearchProject1 = pgTable("phd_course_research_project_1", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    topicOfResearchPractice: text("topic_of_research_practice").notNull(),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+});
+
+export const phdCourseResearchProject2 = pgTable("phd_course_research_project_2", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    topicOfResearchPractice: text("topic_of_research_practice").notNull(),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+});
+
+export const phdCourseResearchPractice = pgTable("phd_course_research_practice", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    topicOfResearchPractice: text("topic_of_research_practice").notNull(),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+});
+
+export const phdCourseReadingCourse2 = pgTable("phd_course_reading_course_2", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    topicOfResearchPractice: text("topic_of_research_practice").notNull(),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+});
+
+export const phdCourseStudyInAdvancedTopics = pgTable("phd_course_study_in_advanced_topics", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    topicOfCourseWork: text("topic_of_course_work").notNull(),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
+});
+
+export const phdCourseDissertations = pgTable("phd_course_dissertations", {
+    erpId: text("erp_id").primaryKey(),
+    campusId: text("campus_id").notNull(),
+    name: text("name").notNull(),
+    instructor: text("instructor"),
+    supervisor: text("supervisor").notNull(),
+    coSupervisor: text("co_supervisor").notNull(),
+    title: text("title").notNull(),
+    midSemMarks: real("mid_sem_marks"),
+    midSemGrade: text("mid_sem_grade"),
+    endSemMarks: real("end_sem_marks"),
+    endSemGrade: text("end_sem_grade"),
 });
